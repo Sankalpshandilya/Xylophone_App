aa7345c0e0157b9ec942062031aaba1411286d4c
SANKALP SHANDILYA <sankalp.bwp.shandilya@gmail.com>
2021-09-06 09:09:39 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/sankalp2/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.sankalp2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audio_cache.dart';
+import 'package:rename/rename.dart';
+
+void main() => runApp(XylophoneApp());
+
+class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    final player = AudioCache();
+    player.play('note$soundNumber.wav');
+  }
+
+  Expanded buildKey({Color color, int soundNumber}) {
+    return Expanded(
+      child: FlatButton(
+        color: color,
+        onPressed: () {
+          playSound(soundNumber);
+        },
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'All Music Note',
+      home: Scaffold(
+        backgroundColor: Colors.black,
+        body: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(color: Colors.red, soundNumber: 1),
+              buildKey(color: Colors.orange, soundNumber: 2),
+              buildKey(color: Colors.yellow, soundNumber: 3),
+              buildKey(color: Colors.green, soundNumber: 4),
+              buildKey(color: Colors.teal, soundNumber: 5),
+              buildKey(color: Colors.blue, soundNumber: 6),
+              buildKey(color: Colors.lightBlue, soundNumber: 7),
+              buildKey(color: Colors.blueAccent, soundNumber: 12),
+              buildKey(color: Colors.cyan, soundNumber: 14),
+              buildKey(color: Colors.cyanAccent, soundNumber: 8),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
